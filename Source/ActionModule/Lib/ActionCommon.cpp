f082ae17db272fdbce54cac36d8a2b5b7f2d7412
CaIzii <2470159730@qq.com>
2025-07-17 10:04:52 -0500

    ( ActionModule ) Feature | ActionCommon: Added utility functions for managing action lifecycle and queries via UActionComponent.

--- Source/ActionModule/Lib/ActionCommon.cpp
+++ Source/ActionModule/Lib/ActionCommon.cpp
@@ -2,3 +2,57 @@
 
 
 #include "ActionCommon.h"
+#include "ActionComponent.h"
+
+FActionResult UActionCommon::AddAction(AActor* componentOwner, TSubclassOf<UAction> actionClass)
+{
+	return GetActionComponent(componentOwner)->AddAction(actionClass);
+}
+
+FActionResult UActionCommon::StartActionByName(AActor* instigator, FName actionName)
+{
+	return GetActionComponent(instigator)->StartActionByName(instigator, actionName);
+}
+
+FActionResult UActionCommon::StartActionByClass(AActor* instigator, TSubclassOf<UAction> actionClass)
+{
+	return GetActionComponent(instigator)->StartActionByClass(instigator, actionClass);
+}
+
+FActionResult UActionCommon::StartActionByType(AActor* instigator, EActionType type)
+{
+	return GetActionComponent(instigator)->StartActionByType(instigator, type);
+}
+
+FActionResult UActionCommon::StopActionByName(AActor* instigator, FName actionName)
+{
+	return GetActionComponent(instigator)->StopActionByName(instigator, actionName);
+}
+
+FActionResult UActionCommon::StopActionByClass(AActor* instigator, TSubclassOf<UAction> actionClass)
+{
+	return GetActionComponent(instigator)->StopActionByClass(instigator, actionClass);
+}
+
+FActionResult UActionCommon::StopActionByType(AActor* instigator, EActionType type)
+{
+	return GetActionComponent(instigator)->StopActionByType(instigator, type);
+}
+
+bool UActionCommon::HasActionByType(AActor* componentOwner, EActionType type)
+{
+	return GetActionComponent(componentOwner)->HasActionByType(type);
+}
+
+UActionComponent* UActionCommon::GetActionComponent(AActor* componentOwner)
+{
+	UActionComponent* comp = Cast<UActionComponent>(componentOwner->GetComponentByClass(UActionComponent::StaticClass()));
+	if (comp)
+		return comp;
+	else
+	{
+		FMessageLog("Blueprint").Warning()
+		                        ->AddToken(FTextToken::Create(FText::FromString(TEXT("Can not find the Action Component"))));
+		return nullptr;
+	}
+}
--- Source/ActionModule/Lib/ActionCommon.h
+++ Source/ActionModule/Lib/ActionCommon.h
@@ -5,12 +5,39 @@
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "ActionCommon.generated.h"
-
+enum class EActionType : uint8;
+class UActionComponent;
+struct FActionResult;
+class UAction;
 /**
- * 
+ * A utility BlueprintFunctionLibrary providing common action handling functionality.
+ * Contains static methods to manage lifecycle and queries of actions associated with an actor.
+ * Methods within this class operate as helpers to interact with a UActionComponent attached to a specified actor.
  */
 UCLASS()
 class ACTIONMODULE_API UActionCommon : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category= Actions)
+	static FActionResult AddAction(AActor* componentOwner, TSubclassOf<UAction> actionClass);
+	UFUNCTION(BlueprintCallable, Category= Actions)
+	static FActionResult StartActionByName(AActor* instigator, FName actionName);
+	UFUNCTION(BlueprintCallable, Category= Actions)
+	static FActionResult StartActionByClass(AActor* instigator, TSubclassOf<UAction> actionClass);
+	UFUNCTION(BlueprintCallable, Category= Actions)
+	static FActionResult StartActionByType(AActor* instigator, EActionType type);
+	UFUNCTION(BlueprintCallable, Category= Actions)
+	static FActionResult StopActionByName(AActor* instigator, FName actionName);
+	UFUNCTION(BlueprintCallable, Category= Actions)
+	static FActionResult StopActionByClass(AActor* instigator, TSubclassOf<UAction> actionClass);
+	UFUNCTION(BlueprintCallable, Category= Actions)
+	static FActionResult StopActionByType(AActor* instigator, EActionType type);
+	UFUNCTION(BlueprintCallable, Category= Actions)
+	static bool HasActionByType(AActor* componentOwner, EActionType type);
+
+private:
+	UFUNCTION(BlueprintCallable, Category= Actions)
+	static UActionComponent* GetActionComponent(AActor* componentOwner);
 };
